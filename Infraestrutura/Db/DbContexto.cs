c4a6d5a4592cc1cf35891707afda7d8f8100bd25
LucasNotaro <lucas.sn@live.com>
2025-08-29 19:07:58 -0300

    feat: configurar conexão MySQL e Entity Framework DbContext

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,5 +1,30 @@
-namespace Infraestrutura.Db;
-public class DbContexto : DbContext
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
 {
-   
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
+public DbSet<Administrador> Administradores { get; set; } = default!;
+
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+    if (!optionsBuilder.IsConfigured)
+    {
+        var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+        if (!string.IsNullOrEmpty(stringConexao))
+        {
+            optionsBuilder.UseMySql(
+                stringConexao,
+                ServerVersion.AutoDetect(stringConexao)
+            );
+        }
+    }
 }
