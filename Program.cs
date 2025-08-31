00799b8c89e81a8b3b094815c159000b49c1d265
LucasNotaro <lucas.sn@live.com>
2025-08-31 00:47:41 -0300

    feat: criar endpoint POST /veiculos para cadastrar veículo

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@ using MinimalApi.Dominio.interfaces;
 using MinimalApi.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
 using MinimalApi.Dominio.ModelViews;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Entidades;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,20 +25,42 @@ builder.Services.AddDbContext<DbContexto>(options => {
 });
 
 var app = builder.Build();
+#endregion
 
-
+#region Home
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-
-app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) => {
     if(administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login com sucesso");
     else
         return Results.Unauthorized();
 });
+#endregion
 
+#region Veículos
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+
+    var veiculo = new Veiculo
+    {
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+    };
+    veiculoServico.Incluir(veiculo);
+    
+    return Results.Created($"/veiculos/{veiculo.Id}", veiculo);
+});
+
+#endregion
+
+#region App
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
